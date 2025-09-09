814301d5f5fa206909c07c3a24c73811cb608bca
dpwl0974 <dpwl0974@gmail.com>
2025-09-09 10:12:16 +0900

    feat: 답변 엔터티 생성

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // 질문 하나 답변 여러개
+    // 즉, Question -> 부모
+    @ManyToOne
+    private Question question;
+
+}
