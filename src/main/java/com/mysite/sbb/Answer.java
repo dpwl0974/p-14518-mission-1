2c96659740f360a5290afeb2184bef91501f59e2
dpwl0974 <dpwl0974@gmail.com>
2025-09-09 10:19:24 +0900

    chore: 어노테이션 수정

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -3,8 +3,6 @@ package com.mysite.sbb;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-
 import java.time.LocalDateTime;
 
 @Getter
