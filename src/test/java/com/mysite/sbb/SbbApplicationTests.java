50b4e0d595f6a9482be00e292c4939da6ba7a8c8
dpwl0974 <dpwl0974@gmail.com>
2025-09-10 09:21:37 +0900

    feat: subject 중 특정 문자열 포함하는 데이터 조회 구현

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -67,4 +67,12 @@ class SbbApplicationTests {
         );
         assertEquals(1, q.getId());
     }
+
+    // findBySubjectLike - subject 중 특정 문자열 포함하는 데이터 조회
+    @Test
+    void testJpa6() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
