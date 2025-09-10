a1f19a2ecb6af324b44e33a0e1c388c70d9dd6c3
dpwl0974 <dpwl0974@gmail.com>
2025-09-10 09:14:15 +0900

    feat: 해당 subject 및 subject&content 데이터 조회 구현

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -52,4 +52,19 @@ class SbbApplicationTests {
         }
     }
 
+    // findBySubject - 해당 subject 데이터 조회
+    @Test
+    void testJpa4() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+    }
+
+    // findBySubjectAndContent - 해당 subject & content 데이터 조회
+    @Test
+    void testJpa5() {
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+        );
+        assertEquals(1, q.getId());
+    }
 }
