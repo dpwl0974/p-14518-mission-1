47b37f6204e1a4010bd46c37d2ce0ea3a979a4c0
dpwl0974 <dpwl0974@gmail.com>
2025-09-14 14:15:10 +0900

    feat: 게시글 상세 페이지 구현

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
+import com.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor // 레포지토리 생성자 방식 주입
 @Service
@@ -12,5 +14,14 @@ public class QuestionService{
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("quesiton not found");
+        }
     }
 }
